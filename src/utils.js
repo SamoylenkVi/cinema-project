9bb9ef42df858f373b436e656eaf43fe84c2ca02
Viktoriia Sevostianova <vikapasha14@gmail.com>
2022-12-15 17:33:11 +0300

    создает функции для отрисовки элемента на странице

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,32 @@ const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
+export const RenderPosition = {
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    default:
+      container.append(element);
+      break;
+  }
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
 const render = (container, template, place) => {
   const renderingHtml = typeof template === 'function' ? template() : template;
 
