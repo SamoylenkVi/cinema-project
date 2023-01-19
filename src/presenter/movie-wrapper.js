95f3466d78330ede8bfc6cd9d01b78a46c118a1b
Viktoriia Sevostianova <vikapasha14@gmail.com>
2023-01-19 15:21:11 +0700

    создает презентер для контейнера с карточками фильмов

---
+++ src/presenter/movie-wrapper.js
@@ -0,0 +1,85 @@
+import EmptyFilmMessageView from '../view/message-empty-film-list';
+import ShowMoreButtonView from '../view/show-more-button';
+import MovieWrapperView from '../view/movie-wrapper';
+import MovieCardPresenter from './movie';
+
+import {
+  renderElement, RenderPosition, remove,
+} from '../utils/render';
+
+const TASK_COUNT_PER_STEP = 5;
+
+export default class MovieList {
+  constructor(filmContainer, classNameSection, title, containerListAttribute) {
+    this._filmContainer = filmContainer;
+    this._emptyFilmMessage = new EmptyFilmMessageView();
+    this._showMoreButton = new ShowMoreButtonView();
+    this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
+
+    this._movieWrapper = new MovieWrapperView(
+      classNameSection,
+      title,
+      containerListAttribute,
+    );
+
+    this._movieWrapperList = null;
+    this._renderedTaskCount = TASK_COUNT_PER_STEP;
+  }
+
+  init(filmCards) {
+    this._filmCards = filmCards.slice();
+
+    this._renderFilmsContainer();
+
+    if (filmCards.length > 0) {
+      this._renderCardsFilm(0, this._renderedTaskCount);
+    } else {
+      this._renderNoFilmsMessage();
+    }
+
+    if (filmCards.length > TASK_COUNT_PER_STEP) {
+      this._renderLoadMoreButton();
+    }
+  }
+
+  _renderCardFilm(film) {
+    const filmPresenter = new MovieCardPresenter(
+      this._movieWrapperList,
+      this._mainElement,
+      this._page,
+    );
+
+    filmPresenter.init(film);
+  }
+
+  _renderCardsFilm(from, to) {
+    this._filmCards
+      .slice(from, to)
+      .forEach((film) => this._renderCardFilm(film));
+  }
+
+  _renderNoFilmsMessage() {
+    renderElement(this._filmContainer, this._emptyFilmMessage, RenderPosition.BEFOREEND);
+  }
+
+  _handleLoadMoreButtonClick() {
+    this._renderCardsFilm(this._renderedTaskCount, this._renderedTaskCount + TASK_COUNT_PER_STEP);
+
+    this._renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (this._renderedTaskCount >= this._filmCards.length) {
+      remove(this._showMoreButton);
+    }
+  }
+
+  _renderLoadMoreButton() {
+    renderElement(this._movieWrapper, this._showMoreButton, RenderPosition.BEFOREEND);
+
+    this._showMoreButton.setClickHandler(this._handleLoadMoreButtonClick);
+  }
+
+  _renderFilmsContainer() {
+    renderElement(this._filmContainer, this._movieWrapper, RenderPosition.BEFOREEND);
+    this._movieWrapperList = this._movieWrapper.getFilmsListContainer();
+  }
+}
