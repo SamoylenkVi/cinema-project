d32d7048ee4b3880a8d59721b3d079c908055135
Viktoriia Sevostianova <vikapasha14@gmail.com>
2023-02-26 16:55:21 +0700

    добавляет модель с методами для сортировки и фильтрации фильмов

--- src/model.js/films.js
+++ src/model.js/films.js
@@ -2,44 +2,82 @@ import Observer from '../utils/observer';
 import { SortType, FilterType } from '../constants';
 import { dateSort, ratingSort } from '../utils/card';
 
+const Filter = {
+  ALL: '',
+  WATCHLIST: 'isWatchList',
+  HISTORY: 'isWatched',
+  FAVORITES: 'isFavorite',
+};
+
 export default class Films extends Observer {
   constructor() {
     super();
-    this._films = [];
-    this._defaultFilms = [];
+    this._sortType = SortType.DEFAULT;
+    this._filterType = FilterType.ALL_MOVIES;
 
-    this._currentSortType = SortType.DEFAULT;
-    this._currentFilterType = FilterType.ALL_MOVIES;
+    this._films = [];
+
+    this._filteredFilms = [];
+    this._filteredAndSortedFilms = [];
   }
 
   set films(films) {
-    this._defaultFilms = films.slice();
-
     this._films = films.slice();
+    this._filterFilms();
+    this._sortFilms();
   }
 
   get films() {
     return this._films;
   }
 
-  set currentSortType(sortType) {
-    this._currentSortType = sortType;
+  get sortType() {
+    return this._sortType;
   }
 
-  get currentSortType() {
-    return this._currentSortType;
+  get filterType() {
+    return this._filterType;
   }
 
-  get currentFilterType() {
-    return this._currentFilterType;
+  _filterFilms() {
+    this._filteredFilms = this._films.filter((film) => {
+      const filterProp = Filter[this._filterType.toUpperCase()];
+      return filterProp ? film[filterProp] : true;
+    });
+  }
+
+  _sortFilms() {
+    let sortByType;
+
+    switch (this._sortType) {
+      case SortType.DATE:
+        sortByType = dateSort;
+        break;
+      case SortType.RATING:
+        sortByType = ratingSort;
+        break;
+      default:
+        sortByType = null;
+        break;
+    }
+
+    if (typeof sortByType === 'function') {
+      this._filteredAndSortedFilms = this._filteredFilms.slice().sort(sortByType);
+    } else {
+      this._filteredAndSortedFilms = this._filteredFilms.slice();
+    }
+  }
+
+  get filteredAndSortedFilms() {
+    return this._filteredAndSortedFilms;
   }
 
   set currentFilterType(filterType) {
-    this._currentFilterType = filterType;
+    this._filterType = filterType;
   }
 
-  updateFilm(updateType, update) {
-    const index = this._films.findIndex((film) => film.id === update.id);
+  _updateFilm(film) {
+    const index = this._films.findIndex((item) => item.id === film.id);
 
     if (index === -1) {
       throw new Error('Can\'t update unexisting task');
@@ -47,27 +85,34 @@ export default class Films extends Observer {
 
     this._films = [
       ...this._films.slice(0, index),
-      update,
+      film,
       ...this._films.slice(index + 1),
     ];
-
-    this._notify(updateType, update);
   }
 
-  sortFilm(updateType, sortType) {
-    switch (sortType) {
-      case (SortType.DATE):
-        this._films = this._films.sort(dateSort);
-        this.currentSortType = SortType.DATE;
-        break;
-      case (SortType.RATING):
-        this._films = this._films.sort(ratingSort);
-        this.currentSortType = SortType.RATING;
-        break;
-      default:
-        this._films = this._defaultFilms;
-        this.currentSortType = SortType.DEFAULT;
-    }
-    this._notify(updateType, this._films);
+  updateFilms(updateType, update) {
+    this._updateFilm(update);
+    this._filterFilms();
+    this._sortFilms();
+
+    this._notify(updateType, update, this._filteredAndSortedFilms);
+  }
+
+  updateFilterFilms(updateType, filterType) {
+    this._sortType = SortType.DEFAULT;
+    this._filterType = filterType;
+
+    this._filterFilms();
+    this._sortFilms();
+
+    this._notify(updateType, this._filteredAndSortedFilms, '');
+  }
+
+  updateSortFilms(updateType, sortType) {
+    this._sortType = sortType;
+
+    this._sortFilms();
+
+    this._notify(updateType, this._filteredAndSortedFilms, '');
   }
 }
